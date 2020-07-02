1d7e993e2ce0c3a22295a09582730d13322c5b86
vlad3942 <vladikgud@mail.ru>
2020-07-03 01:16:14 +0400

    Initial commit

---
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -0,0 +1,27 @@
+package com.example.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting( //Ждёт на вход параметр name, с дифолтом World
+            @RequestParam(name="name", required=false, defaultValue="World") String name,
+            Map<String, Object> model
+    ) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        model.put("some", "Hello Vladik =3");
+        return "main";
+    }
+}
