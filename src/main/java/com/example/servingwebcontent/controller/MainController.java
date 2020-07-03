49f7156b1943836777f2655882d7facfd8a11e75
vlad3942 <vladikgud@mail.ru>
2020-07-03 18:55:36 +0400

    Second Spring lesson, with JPA Hibernate SQL from WebApp and security Login.

--- src/main/java/com/example/servingwebcontent/controller/MainController.java
+++ src/main/java/com/example/servingwebcontent/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.servingwebcontent;
+package com.example.servingwebcontent.controller;
 
 import com.example.servingwebcontent.domain.Message;
 import com.example.servingwebcontent.repos.MessageRepo;
@@ -13,27 +13,25 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
+    @GetMapping("/")
     public String greeting( //Ждёт на вход параметр name, с дифолтом World
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
             Map<String, Object> model
     ) {
-        model.put("name", name);
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
         messageRepo.save(message);
