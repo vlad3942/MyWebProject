bb5a6b9df440c2c3f54e3bda6bcde4eb43b21d0c
vlad3942 <vladikgud@mail.ru>
2020-07-04 21:45:10 +0400

    More

--- src/main/java/com/example/servingwebcontent/domain/Message.java
+++ src/main/java/com/example/servingwebcontent/domain/Message.java
@@ -1,9 +1,6 @@
 package com.example.servingwebcontent.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity //Теперь это сущьность, а не просто класс.
 public class Message {
@@ -14,12 +11,40 @@ public class Message {
     private String text;
     private String tag;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    private String filename;
+
+
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
         this.text = text;
         this.tag = tag;
+        this.author = user;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "<none>";
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 
     public Integer getId() {
